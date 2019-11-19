d29aaf3cc31ba57137f8f561366516d7ff17354e
Ollie Jones <oathompsonjones@gmail.com>
2019-11-19 18:29:19 +0000

    Restructure files

--- index.js
+++
@@ -1,6 +0,0 @@
-const StopWatch = require("./src/JavaScript/StopWatch");
-const DateTime = require("./src/JavaScript/DateTime");
-const TimeSpan = require("./src/JavaScript/TimeSpan");
-const String = require("./src/JavaScript/String");
-const Date = require("./src/JavaScript/Date");
-const Array = require("./src/JavaScript/Array");
