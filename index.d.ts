0644fe85a5215aa46ff887a28826515d919e66b0
Ollie Jones <oathompsonjones@gmail.com>
2019-11-21 17:08:58 +0000

    Manual Index with global Typings

--- index.d.ts
+++ index.d.ts
@@ -1,43 +1,59 @@
-declare class Stopwatch {
-    private startTime;
-    private endTime;
-    start(): void;
-    stop(): void;
-    reset(): void;
-    readonly elapsedMilliseconds: number;
-    readonly elapsedSeconds: number;
-    readonly elapsedMinutes: number;
-    readonly elapsedHours: number;
-    readonly time: {
-        hours: number;
-        minutes: number;
-        seconds: number;
-        milliseconds: number;
-    };
-}
-interface Array<T> {
-    filterByCount(occurances: number): any[];
-    getModes(): any[];
-    getCount(value: any): number;
-    removeDuplicates(): any[];
-    toListString(): string;
-}
-interface Date {
-    getTimes(): {
-        day: string;
-        date: string;
-        month: string;
-        year: string;
-        hours: string;
-        minutes: string;
-        seconds: string;
-        milliseconds: string;
-    };
-    toUptimeString(): string;
-}
-interface String {
-    toProperCase(): string;
-    toTitleCase(): string;
-    toArray(length: number): string[];
-    removeLastIndexOf(str: string): string;
+declare module "node-js-utils" {
+    export class Stopwatch {
+        private startTime: number;
+        private endTime: number;
+        start(): void;
+        stop(): void;
+        reset(): void;
+        toString(): string;
+        readonly elapsedMilliseconds: number;
+        readonly elapsedSeconds: number;
+        readonly elapsedMinutes: number;
+        readonly elapsedHours: number;
+        readonly time: {
+            hours: number;
+            minutes: number;
+            seconds: number;
+            milliseconds: number;
+        };
+    }
+
+    export class Random {
+        private lowerBound: number;
+        private upperBound?: number;
+        constructor(lowerBound: number, upperBound?: number);
+        nextInt(): number;
+    }
+
+    // Typings
+    global {
+        export interface Array<T> {
+            filterByCount(occurances: number): any[];
+            getModes(): any[];
+            getCount(value: any): number;
+            removeDuplicates(): any[];
+            toListString(): string;
+        }
+
+        export interface Date {
+            getTimes(): {
+                day: string;
+                date: string;
+                month: string;
+                year: string;
+                hours: string;
+                minutes: string;
+                seconds: string;
+                milliseconds: string;
+            };
+            toUptimeString(): string;
+        }
+
+        export interface String {
+            toProperCase(): string;
+            toTitleCase(): string;
+            toArray(length: number): string[];
+            removeLastIndexOf(str: string): string;
+        }
+    }
 }
