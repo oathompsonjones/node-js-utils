21f023fe2d9e62b75f3cdb85a954a2d66d222b60
Ollie Jones <oathompsonjones@gmail.com>
2019-11-21 21:16:38 +0000

    Compacted code

--- src/JavaScript/Typings/Date.js
+++ src/JavaScript/Typings/Date.js
@@ -1,70 +1,28 @@
 "use strict";
 Date.prototype.getTimes = function () {
-    let timeMins = this.getUTCMinutes();
-    if (this.getUTCMinutes() <= 9) {
-        timeMins = `0${this.getUTCMinutes()}`;
-    }
-    let timeSecs = this.getUTCSeconds();
-    if (this.getUTCSeconds() <= 9) {
-        timeSecs = `0${this.getUTCSeconds()}`;
-    }
-    let timeMillisecs = this.getUTCMilliseconds();
-    if (this.getUTCMilliseconds() <= 9) {
-        timeMillisecs = `00${this.getUTCMilliseconds()}`;
-    }
-    else if (this.getUTCMilliseconds() <= 99) {
-        timeMillisecs = `0${this.getMilliseconds()}`;
-    }
-    let timeDay = this.getUTCDay();
-    if (timeDay === 0) {
-        timeDay = "Sunday";
-    }
-    else if (timeDay === 1) {
-        timeDay = "Monday";
-    }
-    else if (timeDay === 2) {
-        timeDay = "Tuesday";
-    }
-    else if (timeDay === 3) {
-        timeDay = "Wednesday";
-    }
-    else if (timeDay === 4) {
-        timeDay = "Thursday";
-    }
-    else if (timeDay === 5) {
-        timeDay = "Friday";
-    }
-    else if (timeDay === 6) {
-        timeDay = "Saturday";
-    }
-    const fullTime = {
-        day: timeDay.toString(),
-        date: this.getUTCDate().toString(),
-        month: (this.getUTCMonth() + 1).toString(),
-        year: this.getUTCFullYear().toString(),
-        hours: this.getUTCHours().toString(),
-        minutes: timeMins.toString(),
-        seconds: timeSecs.toString(),
-        milliseconds: timeMillisecs.toString()
-    };
-    return fullTime;
+    const minutes = (this.getUTCMinutes() <= 9 ? `0${this.getUTCMinutes()}` : this.getUTCMinutes()).toString();
+    const seconds = (this.getUTCSeconds() <= 9 ? `0${this.getUTCSeconds()}` : this.getUTCSeconds()).toString();
+    const milliseconds = (this.getUTCMilliseconds() <= 9 ? `00${this.getUTCMilliseconds()}` : this.getUTCMilliseconds() <= 99 ? `0${this.getMilliseconds()}` : this.getUTCMilliseconds()).toString();
+    const day = this.getUTCDay() === 0 ? "Sunday" : this.getUTCDay() === 1 ? "Monday" : this.getUTCDay() === 2 ? "Tuesday" : this.getUTCDay() === 3 ? "Wednesday" : this.getUTCDay() === 4 ? "Thursday" : this.getUTCDay() === 5 ? "Friday" : "Saturday";
+    const time = { day, date: this.getUTCDate().toString(), month: (this.getUTCMonth() + 1).toString(), year: this.getUTCFullYear().toString(), hours: this.getUTCHours().toString(), minutes, seconds, milliseconds };
+    return time;
 };
 Date.prototype.toUptimeString = function () {
-    let timeArray = [`${this.getUTCMilliseconds()}ms`];
+    let time = `${this.getUTCMilliseconds()}ms`;
     if (this.getUTCSeconds() > 0)
-        timeArray = [`${this.getUTCSeconds()}s`, `${this.getUTCMilliseconds()}ms`];
+        time = `${this.getUTCSeconds()}s, ${this.getUTCMilliseconds()}ms`;
     if (this.getUTCMinutes() > 0)
-        timeArray = [`${this.getUTCMinutes()}m`, `${this.getUTCSeconds()}s`, `${this.getUTCMilliseconds()}ms`];
+        time = `${this.getUTCMinutes()}m, ${this.getUTCSeconds()}s, ${this.getUTCMilliseconds()}ms`;
     if (this.getUTCHours() > 0)
-        timeArray = [`${this.getUTCHours()}h`, `${this.getUTCMinutes()}m`, `${this.getUTCSeconds()}s`];
+        time = `${this.getUTCHours()}h, ${this.getUTCMinutes()}m, ${this.getUTCSeconds()}s`;
     if ((this.getUTCDate() - 1) > 0)
-        timeArray = [`${this.getUTCDate() - 1}d`, `${this.getUTCHours()}h`, `${this.getUTCMinutes()}m`];
+        time = `${this.getUTCDate() - 1}d, ${this.getUTCHours()}h, ${this.getUTCMinutes()}m`;
     const weeks = (this.getUTCDate() - 1) >= 7 ? `${Math.floor((this.getUTCDate() - 1) / 7)}w` : "0w";
     if (weeks !== "0w")
-        timeArray = [weeks, `${(this.getUTCDate() - 1) % 7}d`, `${this.getUTCHours()}h`];
+        time = `${weeks}, ${(this.getUTCDate() - 1) % 7}d, ${this.getUTCHours()}h`;
     if (this.getUTCMonth() > 0)
-        timeArray = [`${this.getUTCMonth()}m`, weeks, `${(this.getUTCDate() - 1) % 7}d`, `${this.getUTCHours()}h`];
+        time = `${this.getUTCMonth()}m, ${weeks}, ${(this.getUTCDate() - 1) % 7}d, ${this.getUTCHours()}h`;
     if ((this.getUTCFullYear() - 1970) > 0)
-        timeArray = [`${this.getUTCFullYear() - 1970}y`, `${this.getUTCMonth()}m`, weeks, `${(this.getUTCDate() - 1) % 7}d`];
-    return timeArray.join(", ");
+        time = `${this.getUTCFullYear() - 1970}y, ${this.getUTCMonth()}m, ${weeks}, ${(this.getUTCDate() - 1) % 7}d`;
+    return time;
 };
--- src/TypeScript/Typings/Date.ts
+++ src/TypeScript/Typings/Date.ts
@@ -1,58 +1,23 @@
 Date.prototype.getTimes = function (this: Date): { day: string; date: string; month: string; year: string; hours: string; minutes: string; seconds: string; milliseconds: string; } {
-    let timeMins: string | number = this.getUTCMinutes();
-    if (this.getUTCMinutes() <= 9) {
-        timeMins = `0${this.getUTCMinutes()}`;
-    }
-    let timeSecs: string | number = this.getUTCSeconds();
-    if (this.getUTCSeconds() <= 9) {
-        timeSecs = `0${this.getUTCSeconds()}`;
-    }
-    let timeMillisecs: string | number = this.getUTCMilliseconds();
-    if (this.getUTCMilliseconds() <= 9) {
-        timeMillisecs = `00${this.getUTCMilliseconds()}`;
-    } else if (this.getUTCMilliseconds() <= 99) {
-        timeMillisecs = `0${this.getMilliseconds()}`;
-    }
-    let timeDay: string | number = this.getUTCDay();
-    if (timeDay === 0) {
-        timeDay = "Sunday";
-    } else if (timeDay === 1) {
-        timeDay = "Monday";
-    } else if (timeDay === 2) {
-        timeDay = "Tuesday";
-    } else if (timeDay === 3) {
-        timeDay = "Wednesday";
-    } else if (timeDay === 4) {
-        timeDay = "Thursday";
-    } else if (timeDay === 5) {
-        timeDay = "Friday";
-    } else if (timeDay === 6) {
-        timeDay = "Saturday";
-    }
-    const fullTime = {
-        day: timeDay.toString(),
-        date: this.getUTCDate().toString(),
-        month: (this.getUTCMonth() + 1).toString(),
-        year: this.getUTCFullYear().toString(),
-        hours: this.getUTCHours().toString(),
-        minutes: timeMins.toString(),
-        seconds: timeSecs.toString(),
-        milliseconds: timeMillisecs.toString()
-    };
-    return fullTime;
+    const minutes: string = (this.getUTCMinutes() <= 9 ? `0${this.getUTCMinutes()}` : this.getUTCMinutes()).toString();
+    const seconds: string = (this.getUTCSeconds() <= 9 ? `0${this.getUTCSeconds()}` : this.getUTCSeconds()).toString();
+    const milliseconds: string = (this.getUTCMilliseconds() <= 9 ? `00${this.getUTCMilliseconds()}` : this.getUTCMilliseconds() <= 99 ? `0${this.getMilliseconds()}` : this.getUTCMilliseconds()).toString();
+    const day: string = this.getUTCDay() === 0 ? "Sunday" : this.getUTCDay() === 1 ? "Monday" : this.getUTCDay() === 2 ? "Tuesday" : this.getUTCDay() === 3 ? "Wednesday" : this.getUTCDay() === 4 ? "Thursday" : this.getUTCDay() === 5 ? "Friday" : "Saturday";
+    const time = { day, date: this.getUTCDate().toString(), month: (this.getUTCMonth() + 1).toString(), year: this.getUTCFullYear().toString(), hours: this.getUTCHours().toString(), minutes, seconds, milliseconds };
+    return time;
 };
 
 Date.prototype.toUptimeString = function (this: Date): string {
-    let timeArray: any[] = [`${this.getUTCMilliseconds()}ms`];
-    if (this.getUTCSeconds() > 0) timeArray = [`${this.getUTCSeconds()}s`, `${this.getUTCMilliseconds()}ms`];
-    if (this.getUTCMinutes() > 0) timeArray = [`${this.getUTCMinutes()}m`, `${this.getUTCSeconds()}s`, `${this.getUTCMilliseconds()}ms`];
-    if (this.getUTCHours() > 0) timeArray = [`${this.getUTCHours()}h`, `${this.getUTCMinutes()}m`, `${this.getUTCSeconds()}s`];
-    if ((this.getUTCDate() - 1) > 0) timeArray = [`${this.getUTCDate() - 1}d`, `${this.getUTCHours()}h`, `${this.getUTCMinutes()}m`];
+    let time: string = `${this.getUTCMilliseconds()}ms`;
+    if (this.getUTCSeconds() > 0) time = `${this.getUTCSeconds()}s, ${this.getUTCMilliseconds()}ms`;
+    if (this.getUTCMinutes() > 0) time = `${this.getUTCMinutes()}m, ${this.getUTCSeconds()}s, ${this.getUTCMilliseconds()}ms`;
+    if (this.getUTCHours() > 0) time = `${this.getUTCHours()}h, ${this.getUTCMinutes()}m, ${this.getUTCSeconds()}s`;
+    if ((this.getUTCDate() - 1) > 0) time = `${this.getUTCDate() - 1}d, ${this.getUTCHours()}h, ${this.getUTCMinutes()}m`;
     const weeks = (this.getUTCDate() - 1) >= 7 ? `${Math.floor((this.getUTCDate() - 1) / 7)}w` : "0w";
-    if (weeks !== "0w") timeArray = [weeks, `${(this.getUTCDate() - 1) % 7}d`, `${this.getUTCHours()}h`];
-    if (this.getUTCMonth() > 0) timeArray = [`${this.getUTCMonth()}m`, weeks, `${(this.getUTCDate() - 1) % 7}d`, `${this.getUTCHours()}h`];
-    if ((this.getUTCFullYear() - 1970) > 0) timeArray = [`${this.getUTCFullYear() - 1970}y`, `${this.getUTCMonth()}m`, weeks, `${(this.getUTCDate() - 1) % 7}d`];
-    return timeArray.join(", ");
+    if (weeks !== "0w") time = `${weeks}, ${(this.getUTCDate() - 1) % 7}d, ${this.getUTCHours()}h`;
+    if (this.getUTCMonth() > 0) time = `${this.getUTCMonth()}m, ${weeks}, ${(this.getUTCDate() - 1) % 7}d, ${this.getUTCHours()}h`;
+    if ((this.getUTCFullYear() - 1970) > 0) time = `${this.getUTCFullYear() - 1970}y, ${this.getUTCMonth()}m, ${weeks}, ${(this.getUTCDate() - 1) % 7}d`;
+    return time;
 };
 
 interface Date {
