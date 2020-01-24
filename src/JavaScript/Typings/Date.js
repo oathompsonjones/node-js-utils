8d4b5faf8ea4958eb772c923062d671cdef56cfa
Ollie Jones <oathompsonjones@gmail.com>
2020-01-24 22:31:53 +0000

    Bug fixing!

--- src/JavaScript/Typings/Date.js
+++ src/JavaScript/Typings/Date.js
@@ -12,41 +12,41 @@ Date.prototype.toUptimeString = function (times = []) {
     const weeks = (this.getUTCDate() - 1) >= 7 ? Math.floor((this.getUTCDate() - 1) / 7) : 0;
     if (!times.length) {
         time = `${this.getUTCMilliseconds()}ms`;
-        if (this.getUTCSeconds())
+        if (this.getUTCSeconds() > 0)
             time = `${this.getUTCSeconds()}s, ${this.getUTCMilliseconds()}ms`;
-        if (this.getUTCMinutes())
+        if (this.getUTCMinutes() > 0)
             time = `${this.getUTCMinutes()}m, ${this.getUTCSeconds()}s, ${this.getUTCMilliseconds()}ms`;
-        if (this.getUTCHours())
+        if (this.getUTCHours() > 0)
             time = `${this.getUTCHours()}h, ${this.getUTCMinutes()}m, ${this.getUTCSeconds()}s`;
-        if ((this.getUTCDate() - 1))
+        if ((this.getUTCDate() - 1) > 0)
             time = `${this.getUTCDate() - 1}d, ${this.getUTCHours()}h, ${this.getUTCMinutes()}m`;
-        if (weeks)
+        if (weeks > 0)
             time = `${weeks}w, ${(this.getUTCDate() - 1) % 7}d, ${this.getUTCHours()}h`;
-        if (this.getUTCMonth())
-            time = `${this.getUTCMonth()}mo, ${weeks}, ${(this.getUTCDate() - 1) % 7}d, ${this.getUTCHours()}h`;
-        if ((this.getUTCFullYear() - 1970))
-            time = `${this.getUTCFullYear() - 1970}y, ${this.getUTCMonth()}mo, ${weeks}, ${(this.getUTCDate() - 1) % 7}d`;
+        if (this.getUTCMonth() > 0)
+            time = `${this.getUTCMonth()}mo, ${weeks}w, ${(this.getUTCDate() - 1) % 7}d, ${this.getUTCHours()}h`;
+        if ((this.getUTCFullYear() - 1970) > 0)
+            time = `${this.getUTCFullYear() - 1970}y, ${this.getUTCMonth()}mo, ${weeks}w, ${(this.getUTCDate() - 1) % 7}d`;
     }
     else {
         const arr = [];
         const clean = times.includes("clean");
         if (times.includes("full"))
             times = ["y", "mo", "w", "d", "h", "m", "s", "ms"];
-        if (times.includes("y") && (clean ? (this.getUTCFullYear() - 1970) : true))
+        if (times.includes("y") && (clean ? (this.getUTCFullYear() - 1970) > 0 : true))
             arr.push(`${this.getUTCFullYear() - 1970}y`);
-        if (times.includes("mo") && (clean ? this.getUTCMonth() : true))
+        if (times.includes("mo") && (clean ? this.getUTCMonth() > 0 : true))
             arr.push(`${this.getUTCMonth()}mo`);
-        if (times.includes("w") && (clean ? weeks : true))
+        if (times.includes("w") && (clean ? weeks > 0 : true))
             arr.push(`${weeks}w`);
-        if (times.includes("d") && (clean ? this.getUTCDate() - 1 : true))
+        if (times.includes("d") && (clean ? (this.getUTCDate() - 1) > 0 : true))
             arr.push(`${(this.getUTCDate() - 1) & 7}d`);
-        if (times.includes("h") && (clean ? this.getUTCHours() : true))
+        if (times.includes("h") && (clean ? this.getUTCHours() > 0 : true))
             arr.push(`${this.getUTCHours()}h`);
-        if (times.includes("m") && (clean ? this.getUTCMinutes() : true))
+        if (times.includes("m") && (clean ? this.getUTCMinutes() > 0 : true))
             arr.push(`${this.getUTCMinutes()}m`);
-        if (times.includes("s") && (clean ? this.getUTCSeconds() : true))
+        if (times.includes("s") && (clean ? this.getUTCSeconds() > 0 : true))
             arr.push(`${this.getUTCSeconds()}s`);
-        if (times.includes("ms") && (clean ? this.getUTCMilliseconds() : true))
+        if (times.includes("ms") && (clean ? this.getUTCMilliseconds() > 0 : true))
             arr.push(`${this.getUTCMilliseconds()}ms`);
         time = arr.join(", ");
     }
